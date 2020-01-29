ec06a0cb4706bb1226e80e796249e0a8fbfdaef5
Evan Hong <evangelurtz@gmail.com>
2020-01-29 17:36:43 +0900

    Add controllable player

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravtiy = 9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravtiy;
+        velocity = transform.transform.TransformDirection(velocity);
+        _characterController.Move(velocity * Time.deltaTime);
+    }
+}
