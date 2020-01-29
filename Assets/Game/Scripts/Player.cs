56de499397a3f2debd933418dfec39d10bd23431
Evan Hong <evangelurtz@gmail.com>
2020-01-30 03:38:28 +0900

    Add hitmarker

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -8,17 +8,49 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
     private float _gravtiy = 9.8f;
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        if (_muzzleFlash == null)
+        {
+            Debug.Log("Player: Muzzle Flash is NULL");
+        }
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            _muzzleFlash.SetActive(true);
+            Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayOrigin, out hitInfo, Mathf.Infinity))
+            {
+                Debug.Log("Hit: " + hitInfo.transform.name);
+                GameObject hitMarker = (GameObject)Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(hitMarker, 1f);
+            }
+        } 
+        else 
+        {
+            _muzzleFlash.SetActive(false);
+        }
         CalculateMovement();
+
     }
 
     void CalculateMovement()
